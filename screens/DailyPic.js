a44c984f25d9f4653584a8a5fd86dffff166ccbf
laizamonique <133267385+laizamonique@users.noreply.github.com>
2024-04-08 14:04:32 -0300

    Update DailyPic.js

--- screens/DailyPic.js
+++ screens/DailyPic.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, Image, ImageBackground, TouchableOpacity, StyleSheet, Alert, Platform, StatusBar, SafeAreaView, Linking, ScrollView } from 'react-native';
 
-// import axios from 'axios';
+ import axios from 'axios';
 // import {axios} from 'axios';
 // import axios from axios;
 // import "axios" from axios;
@@ -41,10 +41,10 @@ export default class DailyPicScreen extends Component {
         //borderRadius: 20, margin: 3 }}>
         // </Image>
         
-        // <Image source={{ "uri"= url }} 
-        // style={{ width: "100%", height: 300, 
-        //borderRadius: 20, margin: 3 }}>
-        // </Image>
+        <Image source={{ "uri"= url }} 
+         style={{ width: "100%", height: 300, 
+        borderRadius: 20, margin: 3 }}
+         </Image>
         
         // <Image require={{ "uri": url }} 
         // style={{ width: "100%", height: 300, 
